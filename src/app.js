1909b32b15e7fef79defba8a80baa73eb632d85a
Paul van Motman <pcvanmotman@gmail.com>
2016-05-12 16:32:32 +0200

    finished part 1

--- src/app.js
+++ src/app.js
@@ -27,28 +27,38 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/posted', function (request, response) {
+app.get('/search-user', function (request, response) {
 	response.render("search", {
 	});
 });
 
-app.post('/posted', function (request, response) {
-	console.log("post request received");
-	var searchname = request.body.users[0].toUpperCase() + request.body.users.slice(1);
-	console.log(searchname);
+app.post('/searchresult', function (request, response) {
+	var username = request.body.users[0].toUpperCase() + request.body.users.slice(1);
 
-	for (var i = 0; i < parsedData.length; i++) {
-		if (searchname == parsedData[i].firstname) {
-		response.send("match!!!")
+	console.log("post request received" + username);
+
+	reading.readFile('./users.json', function(parsedData) {
+		var storeUser = []
+		for (var i = 0; i < parsedData.length; i++) {
+			if (username == parsedData[i].firstname || username == parsedData[i].lastname) {
+				storeUser.push(parsedData[i].firstname, parsedData[i].lastname, parsedData[i].email)
+			}
+		};
+		if (storeUser.length !== 0) {
+			response.send("Firstname: " + storeUser[0].toString() + "<br>" + "Lastname: " + storeUser[1].toString() + "<br>" + "Email: " + storeUser[2].toString())
 		}
 		else {
-		response.send("nothing found")
+			response.send("To bad, try again!")
 		}
-	}
+	});
 });
 
+///// Here I execute the code needed for part 2
 
-
+// app.get('/add-users', function (request, response) {
+// 	response.render("add", {
+// 	});
+// });
 
 
 
